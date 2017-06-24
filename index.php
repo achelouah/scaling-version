121de1d7303c673dc4ecae42dcc63fefd00ee7f7
achelouah <achelouah@users.noreply.github.com>
2017-06-24 18:48:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Version v2</p>'; ?>
+ <?php echo '<p>Version v1</p>'; ?>
  <br/> Server IP: <?php echo $_SERVER['SERVER_ADDR']; ?>
  </body>
 </html>
